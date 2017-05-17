eaa2ffceec616c53afdd724b9ec34a73e5975430
kylewstewart <kyle.w.stewart@mac.com>
2017-05-17 11:43:18 -0400

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback) {
+  callback()
+}
+
+function returnsANamedFunction() {
+  return function aFunction() {
+
+  };
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {
+    
+  };
+
+}
